c113883f621c9d8eb91c345db49b3e5c4a206b2b
Chadriax <i14adriacalvet@gmail.com>
2025-07-27 21:51:58 +0200

    Add empty spring application

---
+++ campionsl/src/test/java/com/app/campionsl/CampionslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.campionsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampionslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
