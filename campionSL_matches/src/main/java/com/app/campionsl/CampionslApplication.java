66493e58ca9059e42c7faccd92d6d01655383a43
Chadriax <i14adriacalvet@gmail.com>
2025-08-02 17:05:15 +0200

    Modify name of matches component

---
+++ campionSL_matches/src/main/java/com/app/campionsl/CampionslApplication.java
@@ -0,0 +1,13 @@
+package com.app.campionSL_matches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampionslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampionslApplication.class, args);
+	}
+
+}
---
+++ campionSL_matches/src/test/java/com/app/campionsl/CampionslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.campionSL_matches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampionslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
