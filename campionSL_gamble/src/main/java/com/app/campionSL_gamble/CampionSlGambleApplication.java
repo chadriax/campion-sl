2b5eaf563f6cb04b50385d9ee88f4249774044ce
Rowmi <69115916+Rowmi@users.noreply.github.com>
2025-08-02 18:45:10 +0200

    Initialize Spring Boot project campionSL_gamble
    Add initial Spring Boot application setup with Maven wrapper, project structure, and basic configuration. Includes main application class, test class, and essential dependencies for web and JPA support.

---
+++ campionSL_gamble/src/main/java/com/app/campionSL_gamble/CampionSlGambleApplication.java
@@ -0,0 +1,13 @@
+package com.app.campionSL_gamble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampionSlGambleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampionSlGambleApplication.class, args);
+	}
+
+}
---
+++ campionSL_gamble/src/test/java/com/app/campionSL_gamble/CampionSlGambleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.campionSL_gamble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampionSlGambleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
