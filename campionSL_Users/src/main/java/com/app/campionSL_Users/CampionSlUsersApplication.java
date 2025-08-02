803bb1d432deaee654122f4ad891525c32bfefc7
alpheta25 <lozanolugor46@gmail.com>
2025-08-02 18:52:33 +0200

    Gordo pusheito
    El repositorio del gordo

---
+++ campionSL_Users/src/main/java/com/app/campionSL_Users/CampionSlUsersApplication.java
@@ -0,0 +1,13 @@
+package com.app.campionSL_Users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampionSlUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampionSlUsersApplication.class, args);
+	}
+
+}
---
+++ campionSL_Users/src/test/java/com/app/campionSL_Users/CampionSlUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.campionSL_Users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampionSlUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
