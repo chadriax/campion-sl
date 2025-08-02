d7fb66a1830ac58dd66180664536ec3960b7dd36
Rowmi <69115916+Rowmi@users.noreply.github.com>
2025-08-02 18:01:59 +0200

    Creación Proyecto Apuestas :)

---
+++ campionSL_gamble/src/main/java/com/campionSL/apuestas/Apuestas/ApuestasApplication.java
@@ -0,0 +1,13 @@
+package com.campionSL.apuestas.Apuestas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApuestasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApuestasApplication.class, args);
+	}
+
+}
